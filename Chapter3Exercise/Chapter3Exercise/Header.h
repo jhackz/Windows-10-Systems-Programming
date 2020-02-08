f84db85c32017afac56f3cc35aeca0560e7dbd23
Jason <jason_@tutanota.com>
2020-02-08 08:21:04 -0700

    Windows 10 Systems Programming Chapter 3 exercise

---
+++ Chapter3Exercise/Chapter3Exercise/Header.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#include <stdio.h>
+#include <Windows.h>
+#include <winternl.h>
+
+#pragma comment(lib,"ntdll.lib") 
+
+#if !defined(_WIN64)
+#error The undocumented structures below are only valid for 64-bit code
+#endif // !defined(_WIN64)
+
+// https://github.com/processhacker/phnt/blob/33cfd75a2be59bbde3aa4db3399a9e6bab66ae6a/ntexapi.h
+typedef struct _SYSTEM_PROCESS_INFO
+{
+	ULONG NextEntryOffset;
+	ULONG NumberOfThreads;
+	LARGE_INTEGER WorkingSetPrivateSize; // since VISTA
+	ULONG HardFaultCount; // since WIN7
+	ULONG NumberOfThreadsHighWatermark; // since WIN7
+	ULONGLONG CycleTime; // since WIN7
+	LARGE_INTEGER CreateTime;
+	LARGE_INTEGER UserTime;
+	LARGE_INTEGER KernelTime;
+	UNICODE_STRING ImageName;
+	KPRIORITY BasePriority;
+	HANDLE UniqueProcessId;
+	HANDLE InheritedFromUniqueProcessId;
+	ULONG HandleCount;
+	ULONG SessionId;
+	ULONG_PTR UniqueProcessKey; // since VISTA (requires SystemExtendedProcessInformation)
+	SIZE_T PeakVirtualSize;
+	SIZE_T VirtualSize;
+	ULONG PageFaultCount;
+	SIZE_T PeakWorkingSetSize;
+	SIZE_T WorkingSetSize;
+	SIZE_T QuotaPeakPagedPoolUsage;
+	SIZE_T QuotaPagedPoolUsage;
+	SIZE_T QuotaPeakNonPagedPoolUsage;
+	SIZE_T QuotaNonPagedPoolUsage;
+	SIZE_T PagefileUsage;
+	SIZE_T PeakPagefileUsage;
+	SIZE_T PrivatePageCount;
+	LARGE_INTEGER ReadOperationCount;
+	LARGE_INTEGER WriteOperationCount;
+	LARGE_INTEGER OtherOperationCount;
+	LARGE_INTEGER ReadTransferCount;
+	LARGE_INTEGER WriteTransferCount;
+	LARGE_INTEGER OtherTransferCount;
+	SYSTEM_THREAD_INFORMATION Threads[1]; // SystemProcessInformation
+	// SYSTEM_EXTENDED_THREAD_INFORMATION Threads[1]; // SystemExtendedProcessinformation
+	// SYSTEM_EXTENDED_THREAD_INFORMATION + SYSTEM_PROCESS_INFORMATION_EXTENSION // SystemFullProcessInformation
+} SYSTEM_PROCESS_INFO, * PSYSTEM_PROCESS_INFO;
---
+++ Chapter3Exercise/Chapter3Exercise/Source.cpp
@@ -0,0 +1,108 @@
+#include "Header.h"
+
+BOOL TermProc(DWORD pid)
+{
+	BOOL status = FALSE;
+	HANDLE hProcess = NULL;
+
+	// Get a handle to the process we want terminated
+	hProcess = ::OpenProcess(PROCESS_TERMINATE, FALSE, pid);
+
+	if (hProcess == NULL)
+	{
+		printf("[-] OpenProcess: %08x\n", ::GetLastError());
+	}
+
+	else
+	{
+		// Tell the process "I'll be back"
+		if (::TerminateProcess(hProcess, 0))
+		{
+			// Ensure the process was terminated
+			if (::WaitForSingleObject(hProcess, INFINITE) == WAIT_OBJECT_0)
+			{
+				// This should be set
+				status = TRUE;
+			}
+		}
+
+		else
+		{
+			printf("[-] TerminateProcess: %08x\n", ::GetLastError());
+		}
+	}
+
+	return status;
+}
+
+
+int main()
+{
+	DWORD pid;
+	NTSTATUS status;
+	PSYSTEM_PROCESS_INFO spi;
+	PVOID procBuffer;
+	ULONG returnLength;
+
+
+	// Get our return length with our first call to NtQuerySystemInfromation
+	if (!NT_SUCCESS(status = ::NtQuerySystemInformation(SystemProcessInformation, NULL, NULL, &returnLength)))
+	{
+		// Allocate our buffer of size returnLength here
+		procBuffer = ::VirtualAlloc(NULL, returnLength, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+
+		if (procBuffer == NULL)
+		{
+			printf("[-] VirtualAlloc: %08x\n", ::GetLastError());
+			return -1;
+		}
+
+		spi = (PSYSTEM_PROCESS_INFO)procBuffer;
+		if (!NT_SUCCESS(status = ::NtQuerySystemInformation(SystemProcessInformation, spi, returnLength, NULL)))
+		{
+			printf("[-] NtQuerySystemInformation: %08x\n", status);
+			::VirtualFree(procBuffer, 0, MEM_RELEASE);
+			return -1;
+		}
+
+		// Iterate over the entire list 
+		while (spi->NextEntryOffset) 
+		{
+			printf("[*] %ws\n\t[+] PID: %p\n\t[+] Handle Count: %lu\n\t[+] Session ID: %lu\n\t[+] Threads: %lu\n\t\t\n",
+				spi->ImageName.Buffer, 
+				spi->UniqueProcessId, 
+				spi->HandleCount, 
+				spi->SessionId, 
+				spi->NumberOfThreads); 
+
+			// Pull the start addresses of all the threads per process. 
+			for (ULONG i = 0; i < spi->NumberOfThreads; i++)
+			{
+				printf("\t\t\t[%x] Start Address: %p\n", i, spi->Threads[i].StartAddress);
+			}
+
+			// Get our next entry 
+			spi = (PSYSTEM_PROCESS_INFO)((LPBYTE)spi + spi->NextEntryOffset); 
+		}
+
+		printf("\n[*] Enter a PID to terminate: ");
+		scanf_s("%d", &pid);
+
+		// TerminateProcess wrapper
+		if (!TermProc(pid))
+		{
+			printf("[-] TermProc Failed -_-\n");
+		}
+
+		::VirtualFree(procBuffer, 0, MEM_RELEASE);
+
+	}
+
+	else
+	{
+		printf("[-] NtQuerySystemInformation: %08x\n", status);
+		return -1;
+	}
+
+	return 0;
+}
